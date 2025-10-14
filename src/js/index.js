9644d5456df63e9c1435c8c8bdba8f57a6cb05f1
Bpkarki19 <bipinkarki9861@gmail.com>
2025-10-14 17:40:17 +0545

    refactor layout and styles; implement sidebar toggle functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,17 @@
-import '../scss/style.scss'
+import '../scss/style.scss';
 
+document.addEventListener('DOMContentLoaded', function () {
+  const sidebar = document.getElementById('sidebar');
+  const cancel_button = document.getElementById('cancel-button');
+  const hamburger = document.getElementById('ham-burger');
+  const body = document.body;
 
+  function toggleSidebar() {
+    sidebar.classList.toggle('active');
+    body.classList.toggle('sidebar-open');
+  }
+
+  cancel_button.onclick = toggleSidebar;
+
+  hamburger.onclick = toggleSidebar;
+});
